e95254dbe80645697d1cd80f28559fb29356e5b4
rnosc <rn.oscar11@gmail.com>
2023-10-05 21:04:35 -0600

    Semana 4

---
+++ src/test/java/com/version1/Tienda/ProjectConfig.java
@@ -0,0 +1,39 @@
+
+package com.version1.Tienda;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class ProjectConfig implements WebMvcConfigurer{
+    // Los siguientes metodos son para el cambio de idioma
+    //localeResolver se utiliza para crear una sesion de cambio de idioma 
+    @Bean
+    public LocaleResolver localeResolver(){
+        var slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.getDefault());
+        slr.setLocaleAttributeName("session.current.locale");
+        slr.setTimeZoneAttributeName("session.current.timezone");
+        return slr;
+    }
+    
+    //localeChangeInterceptor se usa para interceptor de cambio de idioma
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+    
+    @Override
+    public void addInterceptors(InterceptorRegistry registro){
+        registro.addInterceptor(localeChangeInterceptor());
+    }
+}
