475321a09ca8ec7e8afd805d6fec1bdc324876a9
0opsyBear <rn.oscar11@gmail.com>
2023-12-07 12:52:21 -0600

    Semana 9

--- src/main/java/com/version1/Tienda/domain/Categoria.java
+++ src/main/java/com/version1/Tienda/domain/Categoria.java
@@ -6,8 +6,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.io.Serializable;
+import java.util.List;
 import lombok.Data;
 
 @Data //indica que va ser una clase de datos
@@ -28,6 +31,10 @@ public class Categoria implements Serializable{
     private String rutaImagen;
     private boolean activo;
 
+    @OneToMany
+    @JoinColumn(name= "id_categoria", updatable = false)
+    List<Producto> productos;
+    
     public Categoria() {
     }
 
--- src/main/java/com/version1/Tienda/service/impl/ProductoServiceImpl.java
+++ src/main/java/com/version1/Tienda/service/impl/ProductoServiceImpl.java
@@ -42,4 +42,25 @@ public class ProductoServiceImpl implements ProductoService {
     public void delete(Producto producto) {
         productoDao.delete(producto);
     }
+    
+    //Consulta JPA para traer info sobre precio inferior y superior 
+    @Override
+    @Transactional(readOnly=true)
+    public List<Producto> findByPrecioBetweenOrderByDescripcion(double precioInf, double precioSup){
+        return productoDao.findByPrecioBetweenOrderByDescripcion(precioInf, precioSup);
+    }
+    
+    //Consulta JPQL para traer info sobre precio inferior y superior 
+    @Override
+    @Transactional(readOnly=true)
+    public List<Producto> metodoJPQL(double precioInf, double precioSup){
+        return productoDao.metodoJPQL(precioInf, precioSup);
+    }
+    
+     //Consulta nativo para traer info sobre precio inferior y superior 
+    @Override
+    @Transactional(readOnly=true)
+    public List<Producto> metodoNativo(double precioInf, double precioSup){
+        return productoDao.metodoJPQL(precioInf, precioSup);
+    }
 }
